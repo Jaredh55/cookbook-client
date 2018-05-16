58c463cf2f78bc050f9069accf576eae63c3ad89
Jaredh55 <jaredh55@gmail.com>
2018-05-15 21:32:48 -0500

    more

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -21,13 +21,17 @@ class Client::RecipesController < ApplicationController
     response = Unirest.post(
                             "http://localhost:3000/api/recipes", parameters: client_params
                             )
-    render 'create.html.erb'
+    # render 'create.html.erb'
+    flash[:success] = "Successfully created recipe"
+    recipe = response.body
+    redirect_to "/client/recipes/#{recipe["id"]}"
   end
 
   def show
     recipe_id = params[:id]
     response = Unirest.get("http://localhost:3000/api/recipes/#{recipe_id}")
     @recipe = response.body #hash
+    # @additional_message = "you made it to show"
     render 'show.html.erb'
   end
 
@@ -50,13 +54,19 @@ class Client::RecipesController < ApplicationController
                              "http://localhost:3000/api/recipes/#{ params[:id] }",
                              parameters: client_params
                             )
-        render 'update.html.erb'
+        # render 'update.html.erb'
+      recipe = response.body
+      flash[:success] = "Successfully updated recipe"
+      redirect_to "/client/recipes/#{recipe["id"]}"
   end
 
   def destroy
     recipe_id = params[:id]
     response = Unirest.delete("http://localhost:3000/api/recipes/#{ recipe_id }")
-    render 'destroy.html.erb'
+    # render 'destroy.html.erb'
+    flash[:success] = "Successfully deleted recipe"
+    redirect_to "/client/recipes"
+
   end
 
 
